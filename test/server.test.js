7476cba806ba830e8fc6c001b28f7915c6d2875d
Therese Brown <browntm96@gmail.com>
2019-08-22 18:53:35 -0700

    Get import statements to work

--- test/server.test.js
+++ test/server.test.js
@@ -1,6 +1,7 @@
 const React = require ('react');
 const { shallow, mount, render } = require ('enzyme');
-const request = require ('request');
+// const request = require ('request');
+import request from 'request';
 // import { exportAllDeclaration } from '@babel/types';
 
 test('Fetch reviews from database', (done) => {

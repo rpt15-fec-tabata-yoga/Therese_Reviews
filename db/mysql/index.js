8eb539467dc48b2f868b81c556f1760be4c7003a
Richard Kang <richardkang1121@gmail.com>
2019-11-06 21:33:22 -0600

    Increase connection timeout to handle Handshake inactivity issue

--- db/mysql/index.js
+++ db/mysql/index.js
@@ -1,11 +1,14 @@
 const mysql = require('mysql');
 
 const pool = mysql.createPool({
+  connectionLimit: 1000,
+  connectTimeout: 60 * 60 * 1000,
+  acquireTimeout: 60 * 60 * 1000,
+  timeout: 60 * 60 * 1000,
   host: 'localhost',
   user: 'root',
   password: 'root',
-  database: 'reviews_db',
-  connectTimeout: 30000
+  database: 'reviews_db'
 });
 
 pool.getConnection(async (err, connection) => {
